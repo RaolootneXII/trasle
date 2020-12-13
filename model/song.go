db628f3feacd6db10b90270d8785e1dbd0191502
RaolootneXII <jv20252025@gmail.com>
2020-12-12 23:06:32 -0300

    adicionando alguns atributos ao model song

--- model/song.go
+++ model/song.go
@@ -5,8 +5,10 @@ import "github.com/jinzhu/gorm"
 // Song - is a song
 type Song struct {
 	gorm.Model
-	Title  string `json:"title" gorm:"not null"`
-	Lyrics string `json:"lyrics" gorm:"not null"`
-	Author string `json:"author" gorm:"not null"`
-	Rating int    `json:"rating" gorm:"not null"`
+	Title      string `json:"title" gorm:"not null"`
+	Lyrics     string `json:"lyrics" gorm:"not null"`
+	Author     string `json:"author" gorm:"not null"`
+	AlbumImage string `json:"image" gorm:"not null"`
+	SongLink   string `json:"song_link" gorm:"not null"`
+	Rating     int    `json:"rating" gorm:"not null"`
 }
